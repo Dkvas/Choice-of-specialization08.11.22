7687a5b5e627f711551c823c3eabc82119dbeb33
Dmitry Kvasov <kvasof.d@gmail.com>
2022-11-08 23:14:51 +0300

    Решена задача с массивом

--- Program/Program.cs
+++ Program/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+Clear();
+
+string[] CreateArray(int size)
+{
+    string[] newArray = new string[size];
+    for (int i = 0; i < newArray.Length; i++)
+    {
+        WriteLine($"Введите {i + 1} элемент массива: ");
+        newArray[i] = Convert.ToString(ReadLine());
+    }
+    return newArray;
+}
+void ShowArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        Write(array[i] + ", ");
+    WriteLine();
+}
+void ThreeSymbolsArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        int len = 3;
+        if (array[i].Length <= len)
+            Write(array[i] + ", ");
+    }
+}
+
+WriteLine("Введиете длинну массива: ");
+int a = Convert.ToInt32(ReadLine());
+string[] myArray = CreateArray(a);
+ShowArray(myArray);
+
+WriteLine("Массив из трёх или менее символов : ");
+ThreeSymbolsArray(myArray);
+WriteLine();
